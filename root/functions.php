32224f9f5e05c703f3dc371be75fcebc7af2a09e
Jeff Behnke <jeff@valid-webs.com>
2013-12-17 01:35:21 -0600

    The start of Compass and directory tree

---
+++ root/functions.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ *
+ * PHP version 5
+ *
+ * Created: 12/17/13, 1:33 AM
+ *
+ * LICENSE: This is PRIVATE source code developed for clients.
+ * It is in no way transferable and copy rights belong to Jeff Behnke @ ValidWebs.com
+ *
+ * @author         Jeff Behnke <code@validwebs.com>
+ * @copyright  (c) 2013 ValidWebs.com
+ *
+ */
+
+
+// End functions.php 
